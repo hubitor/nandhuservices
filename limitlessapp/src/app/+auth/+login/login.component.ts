d310f4607ce9c67ad035e8fe183e84a39f757010
Senthilkumar R <senthil.kumar@limitlesscircle.com>
2017-08-14 12:20:35 +0530

    added super admin role access to stream page

--- limitlessapp/src/app/+auth/+login/login.component.ts
+++ limitlessapp/src/app/+auth/+login/login.component.ts
@@ -67,6 +67,9 @@ export class LoginComponent implements OnInit {
           localStorage.setItem("primary_channel_id", this.loginResponse.primary_channel_id.toString());
           this.getUserRoles(this.loginResponse.user_id);
         } else if(this.loginResponse.user_type === "Super Admin"){
+          localStorage.setItem("broadcaster_id", "1027");
+          localStorage.setItem("w_appname", "dev");
+          localStorage.setItem("primary_channel_id", "20");
           this.router.navigate(['/admin/register']);
         }
       },
