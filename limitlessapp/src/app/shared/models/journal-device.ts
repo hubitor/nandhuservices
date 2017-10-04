cc956f348a8b6a720a818828f9b99cfd83e7792c
uma <uma.raja@limitlesscircle.com>
2017-10-04 10:38:10 +0530

    journaldevice.ts

--- limitlessapp/src/app/shared/models/journal-device.ts
+++ limitlessapp/src/app/shared/models/journal-device.ts
@@ -1,3 +1,15 @@
 
+export class JournalDevice{
+    id: number;
+    journal_setting_id: number;
+    mac_id: string;
+    is_active: boolean;
+    created_by: string;
+    updated_by: string;
+    created_time: string;
+    updated_time: string;
+    
+}
+
     
     
