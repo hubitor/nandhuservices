9a6dde1b5ba08a5762f3f899003f13adc45b2c2a
Senthilkumar R <senthil.kumar@limitlesscircle.com>
2017-11-20 13:59:26 +0530

    added new client file for ads overlay

--- limitlessapp/src/app/shared/models/logo-ads.ts
+++ limitlessapp/src/app/shared/models/logo-ads.ts
@@ -4,6 +4,7 @@ export class LogoAds {
     channel_id: number;
     ad_title: string;
     image_url: string;
+    ftp_path:string;
     is_active: boolean;
     created_by: string;
     updated_by: string;
