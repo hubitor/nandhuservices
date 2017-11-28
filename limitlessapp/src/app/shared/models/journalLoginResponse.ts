410e54cf22a1996267e58113c6735c45dc1f8373
uma <uma.raja@limitlesscircle.com>
2017-11-28 12:10:47 +0530

    journal login response

---
+++ limitlessapp/src/app/shared/models/journalLoginResponse.ts
@@ -0,0 +1,13 @@
+export class JournalLoginResponse{
+    journal_id:number;
+    channel_id:number;
+    first_name:string;
+    last_name:string;
+    active:boolean;
+    user_auth_token:string;
+    auth_token_expire:Date;
+    w_appname:string;
+    primary_channel_id:number;
+    w_get_target_url:string;
+
+}
